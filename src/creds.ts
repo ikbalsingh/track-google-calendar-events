f9e6dc577b3532ee5fb6df5e22f55caa3191dddf
ikbalsingh <ikbalsinghdhanjal23@gmail.com>
2023-09-09 19:24:26 -0700

    1 file modified

--- src/creds.ts
+++ src/creds.ts
@@ -1,2 +1,2 @@
-export const API_KEY = process.env.API_KEY;
-export const CALENDAR_ID = process.env.CALENDAR_ID;
+export const API_KEY = 'AIzaSyDtkGCmNn3QSaaFPfax8HySNbvvVBk7AI8';
+export const CALENDAR_ID = 'ikbalsinghdhanjal23@gmail.com';
